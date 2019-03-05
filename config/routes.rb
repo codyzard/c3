1d6d51402d288e268eb36fdec6a5adaef088e459
HoangTu <Hoang Tu>
2019-03-05 20:13:33 +0700

    Finish Chapter5

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'users/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get  'static_pages/home', to: "static_pages#home" # get ->  to  lay view
-  get  'static_pages/help'
-    get  'static_pages/about'
-    get 'static_pages/contact'
 
-  # root 'application#hello'
-   root 'static_pages#home'
+  # root 'static_pages#home'
+  # get  'static_pages/home', to: "static_pages#home" # get ->  to  lay view
+  # get  'static_pages/help'
+  # get  'static_pages/about'
+  # get 'static_pages/contact
+
+  root "static_pages#home"
+  get  "/help",    to: "static_pages#help" #as: "helf"
+  get  "/about",   to: "static_pages#about"
+  get  "/contact", to: "static_pages#contact"
+  get "/signup", to: "users#new"
 end
---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,8 @@
+# require 'test_helper'
+
+# class ApplicationHelperTest < ActionView::TestCase
+#   test "full title helper" do
+#     assert_equal full_title("Contact"), 5
+#     assert_equal full_title("Help"), 5
+#   end
+# end
