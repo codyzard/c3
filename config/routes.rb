99ac0140e1e0e78d9b500e6bcbf42e7cb65c5a4b
HoangTu <Hoang Tu>
2019-03-06 23:44:53 +0700

    chapter6 model,pw, authenticate

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   # get 'static_pages/contact
 
   root "static_pages#home"
+ # resources :static_pages
   get  "/help",    to: "static_pages#help" #as: "helf"
   get  "/about",   to: "static_pages#about"
   get  "/contact", to: "static_pages#contact"
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_28_160625) do
+ActiveRecord::Schema.define(version: 2019_03_06_161845) do
 
   create_table "static_pages", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
